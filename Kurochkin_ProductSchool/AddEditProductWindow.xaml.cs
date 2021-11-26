b394dd3ab1f059969dbe7e0971aeff32726f2996
Alexander <wafphlez@gmail.com>
2021-11-26 12:07:33 +0300

    init

---
+++ Kurochkin_ProductSchool/AddEditProductWindow.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Kurochkin_ProductSchool
+{
+    /// <summary>
+    /// Interaction logic for AddEditProductWindow.xaml
+    /// </summary>
+    public partial class AddEditProductWindow : Window
+    {
+        bool editMode = false;
+        string photoPath = "";
+        byte[] photo;
+        Product product;
+
+        public AddEditProductWindow()
+        {
+            InitializeComponent();
+            foreach (var item in Helper.entities.Manufacturers.ToList())
+            {
+                manufacturer.Items.Add(item.Name);
+            }
+        }
+
+        public AddEditProductWindow(int id) : this()
+        {
+            editMode = true;
+
+            product = Helper.entities.Products.Where(i => i.ID == id).FirstOrDefault();
+
+            title.Text = product.Title;
+            desc.Text = product.Description;
+            manufacturer.SelectedIndex = product.ManufacturerID.Value;
+            isActive.IsChecked = product.IsActive;
+            price.Text = product.Cost.ToString();
+
+        }
+
+        private void save_Click(object sender, RoutedEventArgs e)
+        {
+            if (editMode)
+            {
+                product.Title = title.Text;
+                product.Description = desc.Text;
+                product.ManufacturerID = manufacturer.SelectedIndex;
+                product.IsActive = isActive.IsChecked.Value;
+                product.Cost = Convert.ToDecimal(price.Text);
+
+
+                Helper.entities.SaveChanges();
+                Close();
+
+                MessageBox.Show("Запись о товаре успешно изменена!", "Done", MessageBoxButton.OK, MessageBoxImage.Information);
+
+
+            }
+            else
+            {
+                product = new Product()
+                {
+                    Title = title.Text,
+                    Description = desc.Text,
+                    ManufacturerID = manufacturer.SelectedIndex,
+                    IsActive = IsActive,
+                    Cost = Convert.ToDecimal(price.Text),
+                    MainImagePath = ""
+
+                };
+
+                Helper.entities.Products.Add(product);
+                Helper.entities.SaveChanges();
+                Close();
+
+                MessageBox.Show("Запись о товаре успешно добавлена!", "Done", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+
+        private void browse_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void isActive_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void isActive_Unchecked(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Kurochkin_ProductSchool/MainWindow.xaml.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Kurochkin_ProductSchool
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Product> list;
+        bool isactive;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            filterManufacturer.Items.Add("Все");
+
+            foreach (var item in Helper.entities.Manufacturers.ToList())
+            {
+                filterManufacturer.Items.Add(item.Name);
+            }
+
+            filterManufacturer.SelectedIndex = 0;
+            list = Helper.entities.Products.ToList();
+            LVProducts.ItemsSource = list;
+        }
+
+        public void Filter()
+        {
+            var data
+                = Helper.entities.Products
+                .Where(i => i.Title.Contains(filterTitle.Text))
+                .Where(i => i.Description.Contains(filterDesc.Text))
+                .Where(i => i.IsActive == isactive).ToList()
+                .ToList();
+
+
+            if (filterManufacturer.SelectedIndex != 0)
+                data = data.Where(i => i.ManufacturerID == filterManufacturer.SelectedIndex).ToList();
+
+            if (sortByComboBoxs.SelectedIndex == 0)
+                LVProducts.ItemsSource = data.OrderByDescending(i => i.Cost).ToList();
+            else
+                LVProducts.ItemsSource = data.OrderBy(i => i.Cost).ToList();
+        }
+
+        private void filterManufacturer_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Filter();
+        }
+
+        private void filterTitle_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Filter();
+
+        }
+
+        private void filterDesc_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+            Filter();
+        }
+
+        private void clearFilters_Click(object sender, RoutedEventArgs e)
+        {
+            filterTitle.Text = "";
+            filterDesc.Text = "";
+            filterManufacturer.SelectedIndex = 0;
+            filterIsActive.IsChecked = false;
+
+            Filter();
+        }
+
+        private void viewSellHistory_Click(object sender, RoutedEventArgs e)
+        {
+            if (LVProducts.SelectedItems.Count == 0)
+            {
+                MessageBox.Show("Ничего не выбрано!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+        }
+
+        private void deleteProduct_Click(object sender, RoutedEventArgs e)
+        {
+            if (LVProducts.SelectedItems.Count == 0)
+            {
+                MessageBox.Show("Ничего не выбрано!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            if (MessageBox.Show("Вы уверены?", "Подтвердите удаление", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                foreach (Product item in LVProducts.SelectedItems)
+                {
+                    Helper.entities.Products.Remove(item);
+                }
+
+                MessageBox.Show("Выбранные данные были успешно удалены!", "Done", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                Helper.entities.SaveChanges();
+            }
+            Filter();
+        }
+
+        private void editProduct_Click(object sender, RoutedEventArgs e)
+        {
+            if (LVProducts.SelectedItems.Count == 0)
+            {
+                MessageBox.Show("Выделение пустое!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            new AddEditProductWindow(((Product)LVProducts.SelectedItem).ID).ShowDialog();
+            Filter();
+        }
+
+        private void addProduct_Click(object sender, RoutedEventArgs e)
+        {
+            new AddEditProductWindow().ShowDialog();
+            Helper.entities = new Entities();
+            Filter();
+        }
+
+        private void filterIsActive_Checked(object sender, RoutedEventArgs e)
+        {
+            isactive = true;
+            Filter();
+
+        }
+
+        private void nextPage_Click(object sender, RoutedEventArgs e)
+        {
+            isactive = false;
+            Filter();
+        }
+    }
+}
